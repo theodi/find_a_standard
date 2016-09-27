9318ea790c114ed354b0ec59631b216fbb4e3ad4
pezholio <pezholio@gmail.com>
2016-09-27 11:40:15 +0100

    Get page title

--- lib/find_a_standard/index.rb
+++ lib/find_a_standard/index.rb
@@ -8,9 +8,16 @@ module FindAStandard
     private
 
       def page_text
+        html.at_css('body').text.gsub(/\s+/, ' ').strip
+      end
+
+      def page_title
+        html.at_css('title').text.gsub(/\s+/, ' ').strip
+      end
+
+      def html
         body = open(@url).read
         html = Oga.parse_html(body)
-        html.at_css('body').text.gsub(/\s+/, ' ').strip
       end
 
   end
--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -8,7 +8,16 @@ describe FindAStandard::Index do
 
     index = described_class.new('http://www.example.com')
 
-    expect(index.send(:page_text)).to match (/Example Domain This domain is established to be used/)
+    expect(index.send(:page_text)).to match /Example Domain This domain is established to be used/
+  end
+
+  it 'extracts the title from a webpage' do
+    stub_request(:get, "www.example.com").
+      to_return(body: File.read(File.join 'spec', 'fixtures', 'index.html'))
+
+    index = described_class.new('http://www.example.com')
+
+    expect(index.send(:page_title)).to eq('Example Domain')
   end
 
 end
