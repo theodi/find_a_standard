69415f144432e1d40c6d8c303d72eb9a2a5d06e1
pezholio <pezholio@gmail.com>
2016-09-27 13:48:47 +0100

    Connect to Elasticsearch

---
+++ lib/find_a_standard/client.rb
@@ -0,0 +1,42 @@
+module FindAStandard
+  class Client
+
+    INDEX_NAME = 'find_a_standard'
+
+    def self.index(title, body)
+      body = {
+        title: title,
+        body: body
+      }
+      connection.index(index: INDEX_NAME, type: 'standard', body: body)
+    end
+
+    def self.search(query)
+      body = {
+        query: {
+          match: {
+            '_all' => query
+          }
+        }
+      }
+      connection.search(index: INDEX_NAME, body: body)
+    end
+
+    def self.create_index
+      connection.indices.create(index: INDEX_NAME)
+    end
+
+    def self.delete_index
+      connection.indices.delete(index: INDEX_NAME)
+    end
+
+    def self.refresh_index
+      connection.indices.refresh(index: INDEX_NAME)
+    end
+
+    def self.connection
+      @@connection ||= Elasticsearch::Client.new url: ENV['ES_URL']
+    end
+
+  end
+end
