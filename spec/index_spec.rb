274ddd5aefb0b42224660c268a8a6152e8439350
pezholio <pezholio@gmail.com>
2016-09-27 11:41:06 +0100

    Refactor tests

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -2,22 +2,19 @@ require 'spec_helper'
 
 describe FindAStandard::Index do
 
-  it 'extracts the text from a webpage' do
+  before(:each) do
     stub_request(:get, "www.example.com").
       to_return(body: File.read(File.join 'spec', 'fixtures', 'index.html'))
 
-    index = described_class.new('http://www.example.com')
+    @index = described_class.new('http://www.example.com')
+  end
 
-    expect(index.send(:page_text)).to match /Example Domain This domain is established to be used/
+  it 'extracts the text from a webpage' do
+    expect(@index.send(:page_text)).to match /Example Domain This domain is established to be used/
   end
 
   it 'extracts the title from a webpage' do
-    stub_request(:get, "www.example.com").
-      to_return(body: File.read(File.join 'spec', 'fixtures', 'index.html'))
-
-    index = described_class.new('http://www.example.com')
-
-    expect(index.send(:page_title)).to eq('Example Domain')
+    expect(@index.send(:page_title)).to eq('Example Domain')
   end
 
 end
